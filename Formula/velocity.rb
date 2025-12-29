c4bb35c6f666ef94e2a7c71b749f2e186800d6c7
Ali Raza <brainlet.ali@gmail.com>
2025-12-29 17:22:18 +0500

    velocity 0.1.4

--- Formula/velocity.rb
+++ Formula/velocity.rb
@@ -1,17 +1,17 @@
 class Velocity < Formula
   desc "CLI for the Velocity Go web framework"
   homepage "https://github.com/velocitykode/velocity-cli"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/velocitykode/velocity-cli/releases/download/v0.1.3/velocity-darwin-arm64.tar.gz"
-      sha256 "9cb6e3bcd87bf04ad28412fe6e64715e2e5be7b962db8303589c5d353fed4918"
+      url "https://github.com/velocitykode/velocity-cli/releases/download/v0.1.4/velocity-darwin-arm64.tar.gz"
+      sha256 "0bd479c7e5c4be7e9da5f588264df6f99655f8262ba3bb04eefde470f400014d"
     end
     on_intel do
-      url "https://github.com/velocitykode/velocity-cli/releases/download/v0.1.3/velocity-darwin-amd64.tar.gz"
-      sha256 "277b2a10c9b196e072c445275796ce879538e2ae093abfbfaaba0b79385f946e"
+      url "https://github.com/velocitykode/velocity-cli/releases/download/v0.1.4/velocity-darwin-amd64.tar.gz"
+      sha256 "4600befa1ef33ab9fec7a89a3c478aa396fcf989bc6a3baccd17af51debba713"
     end
   end
 
